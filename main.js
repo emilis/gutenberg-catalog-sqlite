5f0d8737c58ecfe65374f0377498109609d3d29b
Emilis Dambauskas <emilis.d@gmail.com>
2011-07-26 16:33:54 +0300

    Restructured main.js.

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 
-// Set up require and JAR paths:
-addToClasspath(module.directory + "/jars/");
+// Set up require paths:
 require.paths.push(module.directory);
 
+// requirements:
+var fs = require("fs");
+
+// add all jars:
+var jar_dir = module.directory + "/jars/";
+for each (var fname in fs.list(jar_dir)) {
+    addToClasspath(jar_dir + fname);
+}
 
 
 // --- Main: ---
 
-// paths:
-var data_dir = module.directory + "/data";
-var catalog_file = data_dir + "/catalog.rdf";
-var db = data_dir + "/catalog.sqlite";
+function main() {
+    return;
+    // paths:
+    var data_dir = module.directory + "/data";
+    var catalog_file = data_dir + "/catalog.rdf";
+    var db = data_dir + "/catalog.sqlite";
 
-// vars:
-var parser = require("ctl/sax_parser");
-var gutenberg_handler = require("gutenberg_handler")
+    // vars:
+    var parser = require("ctl/sax_parser");
+    var gutenberg_handler = require("gutenberg_handler")
 
-// parse:
-parser.setContentHandler(gutenberg_handler);
-parser.setFileName(catalog_dile);
-parser.parse();
+    // parse:
+    parser.setContentHandler(gutenberg_handler);
+    parser.setFileName(catalog_dile);
+    parser.parse();
 
-print("Parsing finished.");
+    print("Parsing finished.");
+}
 
 
